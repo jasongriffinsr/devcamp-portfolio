1884d4fe97d11a59070839e2cb8cfb8c22f543ce
Jason Griffin <jasonjgriffin@gmail.com>
2020-07-06 11:45:18 -0500

    Implemented destory action for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -33,12 +33,22 @@ def update
         format.html { render :edit }
       end
     end
-end
+end 
 
 def show
  @portfolio_item = Portfolio.find(params[:id])
-
 end
 
+def destroy
+		# Perform the lookup
+		@portfolio_item = Portfolio.find(params[:id])
 
+		# Destroy/delete the record
+		@portfolio_item.destroy
+
+		# Redirect
+	    respond_to do |format|
+	      format.html { redirect_to portfolios_url, notice: 'The record was removed.' }
+		end
+	end
 end
