da7fbab59451e0fc2e60b1eb34f50df0f827358a
Jason Griffin <jasonjgriffin@gmail.com>
2020-07-10 13:58:21 -0500

    Implemented authorization engine for portfolio items and blogs

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,7 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout 'portfolio'
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
 
 	def index
 		@portfolio_items = Portfolio.all
@@ -28,7 +29,7 @@ class PortfoliosController < ApplicationController
   end
 
 def edit
-  binding.pry
+  
 end 
 
 def update
